5ec4fe99f31c623e8b4b5e675a8639a7be095dd5
carlos <c.a.ramirez012@gmail.com>
2020-11-23 05:50:59 -0300

    refactorización del código

---
+++ bd.py
@@ -0,0 +1,195 @@
+import sys
+import os
+import pymysql
+import pandas as pd
+from numpy import *
+host = os.getenv('localhost')
+port = os.getenv('MYSQL_PORT')
+user = os.getenv('MYSQL_USER')
+password = os.getenv('MYSQL_PASSWORD')
+database = os.getenv('MYSQL_DATABASE')
+
+
+conn = pymysql.connect(
+    host = host,
+    port = int(3306),
+    user = "root",
+    passwd ="",
+    db = "recommender_system",
+    charset = 'utf8mb4')
+
+# # [BD]Consulta   si una lista esta registrada con metadata
+    
+
+def existeListaPorId( idImdb ):
+
+    exist = True
+
+    sql  = " SELECT * FROM lists WHERE idImdb = %s "
+
+    with conn.cursor() as cursor:
+
+        row_count = cursor.execute( sql, (idImdb) )
+
+        if row_count == 0:
+
+            exist = False
+
+    return exist
+
+# # [BD] Comprueba si una lista está registrada en List_tittlelist
+
+def existeEnListTittleList( idImdb ):
+
+    exist = True
+
+    sql  = " SELECT * FROM list_tittlelist WHERE id_list = %s "
+
+    with conn.cursor() as cursor:
+
+        row_count = cursor.execute( sql, ( idImdb ) )
+
+        if row_count == 0:
+
+            exist = False
+
+    return exist
+
+# # [BD] Consulta si un tittle (pelicula) existe
+
+def existeTittlePorId( tconst ):
+
+    exist = True
+
+    sql  = " SELECT * FROM tittle_list WHERE tconst = %s "
+
+    with conn.cursor() as cursor:
+
+        row_count = cursor.execute( sql, ( tconst ) )
+
+        if row_count == 0:
+
+            exist = False
+
+    return exist
+
+# # [BD] Insertar información descriptiva de una lista
+
+def instertarLista( name, link, metadata, idImdb ):
+
+    with conn.cursor() as cursor:
+
+        # CREA UNA NUEVA FILA
+
+        sql = "INSERT INTO lists ( name, link, metadata, idImdb ) VALUES ( %s, %s, %s,%s )"
+
+        cursor.execute( sql,( name, link, metadata, idImdb ) )
+
+        #GUARDAMOS LOS CAMBIOS
+
+        conn.commit()
+
+def existeListTittleList(id_list, id_tittlelist):
+
+    exist = True
+
+    sql  = "SELECT * FROM list_tittlelist where id_list =%s AND id_tittlelist =%s"
+
+    with conn.cursor() as cursor:
+
+        row_count = cursor.execute( sql, ( id_list, id_tittlelist ) )
+
+        if row_count == 0:
+
+            exist = False
+
+    return exist
+    
+# # [BD] Inserta asosiciaion pelicula-lista (tabla n:n)
+
+def instertarListTittleList( id_list, id_tittlelist):
+    
+    with conn.cursor() as cursor:
+
+        # CREA UNA NUEVA FILA
+
+        sql = "INSERT INTO list_tittlelist ( id_list, id_tittlelist ) VALUES ( %s, %s )"
+
+        cursor.execute( sql,( id_list, id_tittlelist ) )
+
+        #GUARDAMOS LOS CAMBIOS
+
+        conn.commit()
+        print("\n[BD]Guardando asociación id_list[%s] id_tittlelist[%s]" %(id_list, id_tittlelist ))
+
+# # [BD] Inserta información de peliculas
+
+def instertarTittleList( tconst, f_created, f_modified, description, name_tittle, url, tittle_type, imdb_rating, runtime_mins, year, release_date):
+
+    with conn.cursor() as cursor:
+
+        # CREA UNA NUEVA FILA
+
+        sql = "INSERT INTO tittle_list ( tconst, f_created, f_modified, description, name_tittle, url, tittle_type, imdb_rating, runtime_mins, year, release_date ) VALUES ( %s, %s, %s,%s, %s, %s,%s, %s, %s,%s, %s)"
+
+        cursor.execute( sql,( tconst, f_created, f_modified, description, name_tittle, url, tittle_type, imdb_rating, runtime_mins, year, release_date ) )
+
+
+        #GUARDAMOS LOS CAMBIOS
+
+        conn.commit()
+
+# # [BD] OBTENGO TODAS LAS PELICULAS DE UNA LISTA
+
+def obtenerTituloDeLista(idList):
+
+    sql  = " SELECT * FROM list_tittlelist WHERE id_list = %s "
+
+    with conn.cursor() as cursor:
+
+        result = cursor.execute( sql, ( idList ) )
+
+        if result == 0:
+
+            return -1
+
+        result= cursor.fetchall()
+
+    return result
+
+# # [BD] Buscar si una pelicula dentro de una lista existe
+
+def validarPeliculaEnLista( idList, idTitleList ):
+
+    exist = True
+
+    sql  = "SELECT * FROM list_tittlelist where id_list=%s AND id_tittlelist=%s"
+
+    with conn.cursor() as cursor:
+
+        row_count = cursor.execute( sql, ( idList, idTitleList ) )
+
+        if row_count == 0:
+
+            exist = False
+
+    return exist
+
+# # [BD] Total de peliculas guardadas (S)
+
+
+def totalDeTitulosGuardados(  ):
+
+    sql  = "SELECT COUNT(DISTINCT(tconst)) FROM tittle_list "
+
+    with conn.cursor() as cursor:
+
+        cursor.execute( sql )
+
+        value = cursor.fetchall()
+
+        if len(value) <= 0:
+
+            return None
+
+    return value
---
+++ rs.py
@@ -0,0 +1,99 @@
+from bd import *
+from scrapper import *
+
+
+# # [RS] Compara peliculas de una lista en una lista dada
+
+def inicioCompararListas( idOriginList , idEndList ):
+    
+    # input idOriginList: ID de la lista que se quiere comparar || ej: "ls096276650"
+    
+    #       idEndList: ID unica o arreglo con los que se quiere comparar || 
+    
+    #       ej (1) "ls021428038" 
+    
+    #       ej (2) [ "ls021428038", "ls094566741" ]
+    
+    print("\n **********************************************************")
+    
+    print("\n Comenzando proceso de comparación")
+    
+    originTitles = obtenerTituloDeLista( idOriginList )
+    
+    
+    result = []
+    
+    numIterator = 1
+    
+    if isinstance( idEndList , list ):                
+        
+        numIterator = len( idEndList ) 
+        
+        for i in range( numIterator ):
+            
+            auxMatrix = compararDosListas( originTitles, idEndList[ i ] )            
+                    
+            result.append( auxMatrix )
+            
+    else:
+         
+            auxMatrix = compararDosListas( originTitles, idEndList )            
+                    
+            result.append( auxMatrix )
+    
+    print("\n Fin de proceso de comparación ")
+    
+    print("\n **********************************************************")
+    
+    return result
+
+def compararDosListas( originTitles, endTitle):
+    
+    # input originTitles: Lista que tiene todos los titulos de una lista || ej:  ( (12196, 'ls096276650', 'tt5503686'), (12197, 'ls096276650', 'tt8722346') )
+    
+    #       idEndList: ID unica o arreglo con los que se quiere comparar ||      ej  "ls021428038" 
+    
+    result = []
+    
+    totalTitlesFounds = 0
+    
+    #result.insert( 0, totalTitlesFounds )
+    
+    for oTitle in originTitles: 
+            
+        if validarPeliculaEnLista( endTitle  , oTitle ):                    
+
+            #result.append( 1 )
+            print("oTitle: ", oTitle)
+
+            totalTitlesFounds += 1
+
+        #else:
+            
+                    # result.append( 0 )
+                
+    result.append( totalTitlesFounds ) 
+    
+    print( "\n Número de titulos contenidos en la lista: ", totalTitlesFounds)
+    
+    print(" \n Arreglo -> ", result )
+    
+    return result
+
+def modelo( S, sub, L, n ):
+    
+    # S -> Num de peliculas de S
+    
+    # sub -> numero de peliculas de S'
+    
+    # n -> numero de peliculas que se encuentran en L
+    
+    # L -> numero de peliculas de L
+    
+    PA = n / sub
+    
+    num = S * PA
+    
+    resul = num / L
+    
+    print("\n Resultado del modelo: ", resul)
